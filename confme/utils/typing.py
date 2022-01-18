ffe1b763d733beb5650c1af3e2445da73fbd7b44
Iwan Bolzern <iwan.bolzern@roche.com>
2022-01-18 16:20:51 +0100

    fixed issue with pydantic 1.9

--- confme/utils/typing.py
+++ confme/utils/typing.py
@@ -6,12 +6,15 @@ from pydantic.schema import model_schema
 
 def _create_dict(schema_head: Dict, definitions: Dict):
     schema_dict = {}
-    for key, value in schema_head['properties'].items():
-        if 'allOf' in value:
-            sub_schema = value['allOf'][0]['$ref'].replace('#/definitions/', '')
-            schema_dict[key] = _create_dict(definitions[sub_schema], definitions)
-        else:
-            schema_dict[key] = None
+    if 'type' in schema_head and schema_head['type'] == 'object':
+        for key, value in schema_head['properties'].items():
+            if 'allOf' in value:
+                sub_schema = value['allOf'][0]['$ref'].replace('#/definitions/', '')
+                schema_dict[key] = _create_dict(definitions[sub_schema], definitions)
+            else:
+                schema_dict[key] = None
+    else:
+        return None
     return schema_dict
 
 
